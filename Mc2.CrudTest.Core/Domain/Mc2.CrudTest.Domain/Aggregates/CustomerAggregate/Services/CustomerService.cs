e2471cb13f362043206863df32a86b28a56a2a3b
ghardashpoor-m <mahdi.ghardashpoor@gmail.com>
2024-01-11 16:12:44 +0330

    feat: add domain services

---
+++ Mc2.CrudTest.Core/Domain/Mc2.CrudTest.Domain/Aggregates/CustomerAggregate/Services/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using Mc2.CrudTest.Domain.Abstractions.DomainServices;
+using Mc2.CrudTest.Domain.Abstractions.ExternalServices;
+
+namespace Mc2.CrudTest.Domain.Aggregates.CustomerAggregate.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IPhoneNumberValidator _phoneNumberValidator;
+        private readonly IBankAccountNumberValidator _bankAccountNumberValidator;
+
+        public CustomerService(IPhoneNumberValidator phoneNumberValidator, IBankAccountNumberValidator bankAccountNumberValidator)
+        {
+            _phoneNumberValidator = phoneNumberValidator;
+            _bankAccountNumberValidator = bankAccountNumberValidator;
+        }
+
+        public bool ValidateBankAccountNumber(string bankAccountNumber, out string? message)
+        {
+            return _bankAccountNumberValidator.IsValid(bankAccountNumber, out message);
+        }
+
+        public bool ValidatePhoneNumber(string phoneNumber, out string? message)
+        {
+            return _phoneNumberValidator.IsValid(phoneNumber, out message);
+        }
+    }
+}
